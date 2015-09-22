973d1a3d19374b7a3e24cc760d7b338f1a71a50a
Thomas Torsney-Weir <torsneyt@gmail.com>
2015-09-22 09:21:22 +0200

    switch from codeship to travis

--- codeship/devtools_setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-mkdir $R_LIBS
-Rscript -e 'install.packages(c("XML", "RCurl"), repos="http://cran.rstudio.com/")'
-wget https://cran.r-project.org/src/contrib/Archive/rversions/rversions_1.0.0.tar.gz
-Rscript -e 'install.packages("rversions_1.0.0.tar.gz", repos=NULL, type="source")'
-Rscript -e 'install.packages("devtools", repos="http://cran.rstudio.com/")'
-
--- codeship/testthat_setup.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-wget https://cran.r-project.org/src/contrib/Archive/testthat/testthat_0.8.1.tar.gz
-Rscript -e 'install.packages("testthat_0.8.1.tar.gz")'
