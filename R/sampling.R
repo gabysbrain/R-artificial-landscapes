d5c6e489c2382d5588d2d362c955624d596b6fc6
Thomas Torsney-Weir <torsneyt@gmail.com>
2017-03-29 12:42:01 +0200

    update docs to get package working

--- R/sampling.R
+++ R/sampling.R
@@ -3,27 +3,47 @@ library(lhs)
 library(randtoolbox)
 library(stringr)
 
-# The sampling function takes (n, k) where n is the number of samples
-# and k is the number of dimensions. The sampling functions should return
-# samples in a 0-1 hypercube.
-# sampling.func <- function(n, k)
+#' The sampling functions take (n, k) where n is the number of samples
+#' and k is the number of dimensions. The sampling functions should return
+#' samples in a 0-1 hypercube.
+#' sampling.func <- function(n, k)
+#'
+#' @param n The number of samples.
+#' @param k The number of dimensions.
+#' @param ... Additional arguments for the sampling (e.g. \code{seed}).
+#' @name samplingf
+NULL
 
-lh.sample <- function(n,k) maximinLHS(n, k)
-cartesian.sample <- function(n,k) {
+#' @rdname samplingf
+#' @export
+lh.sample <- function(n,k,...) lhs::maximinLHS(n, k)
+#' @rdname samplingf
+#' @export
+cartesian.sample <- function(n,k,...) {
   nums <- seq(0, 1, length.out=n)
   do.call(expand.grid, rep(list(nums), k))
 }
-random.sample <- function(n,k) matrix(runif(n*k), nrow=n, ncol=k)
-hexagonal.sample <- function(n,k) {
+#' @rdname samplingf
+#' @export
+random.sample <- function(n,k,...) matrix(runif(n*k), nrow=n, ncol=k)
+#' @rdname samplingf
+#' @export
+hexagonal.sample <- function(n,k,...) {
   if(k != 2) stop("cannot do hexagonal sampling on k!=2 dimensions")
   c <- cartesian.sample(n, 2)
   V <- array(c(1, 0, 0.5, sqrt(3)/2), c(2,2))
   pts <- t(as.matrix(V %*% t(c)))
   unique(pts %% 1) # wrap everything
 }
-torus.sample <- function(n,k,...) torus(n, k, ...)
-sobol.sample <- function(n,k,...) sobol(n, k, ...)
-halton.sample <- function(n,k,...) halton(n, k, ...)
+#' @rdname samplingf
+#' @export
+torus.sample <- function(n,k,...) randtoolbox::torus(n, k, ...)
+#' @rdname samplingf
+#' @export
+sobol.sample <- function(n,k,...) randtoolbox::sobol(n, k, ...)
+#' @rdname samplingf
+#' @export
+halton.sample <- function(n,k,...) randtoolbox::halton(n, k, ...)
 
 map.range <- function(x, rng.min, rng.max, dom.min=0, dom.max=1) {
   (x-dom.min) / (dom.max-dom.min) * (rng.max-rng.min) + rng.min
@@ -52,8 +72,8 @@ sample.func <- function(FUN.name, n, k, method="lh.sample", ...) {
   X <- map.range(X, func.domain$min, func.domain$max)
   Y <- func(X, ...)
   X.df <- data.frame(X)
-  names(X.df) <- str_c("x", 1:k)
+  names(X.df) <- stringr::str_c("x", 1:k)
   Y.df <- data.frame(Y)
-  names(Y.df) <- str_c("y", 1:ncol(Y.df))
+  names(Y.df) <- stringr::str_c("y", 1:ncol(Y.df))
   cbind(X.df, Y.df)
 }
